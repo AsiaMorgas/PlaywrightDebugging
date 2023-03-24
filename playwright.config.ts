725e84a502eb14e1d2a89f882c123fe56cb3ddb3
JoannaMorgas <asia.morgas@gmail.com>
2023-03-24 23:49:08 +0100

    change reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : 1,
     /* ------------> REPORTERS <------------ */
-    reporter: process.env.CI ? [['html'], ['github'], ['line']] : [['html'], ['line']],
+    reporter: process.env.CI ? [['html'], ['list']] : [['html'], ['list']],
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: 'https://www.backbase.com',
